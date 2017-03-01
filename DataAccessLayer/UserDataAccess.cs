a49c7be9f6720ecddebf56bf7dc6cf031c6156c6
Britt <you@example.com>
2017-02-28 23:08:56 -0500

    Finished CreateUser Method and added Params from SP_RegisterUser

--- DataAccessLayer/UserDataAccess.cs
+++ DataAccessLayer/UserDataAccess.cs
@@ -16,23 +16,43 @@ namespace DataAccessLayer
         string _connection = ConfigurationManager.ConnectionStrings["dbConnection"].ConnectionString;
         string _FileLocation = @"C:/Users/Admin2/Desktop/ErrorLog.txt";
 
-        public void CreateUser(DataUser daUser)
+        public void CreateUser(DataUser daUser) //registers a user
         {
             SqlConnection lConnection = new SqlConnection(_connection);
-            //SQLCommand cmd = new SQLCommand("", lConnection);
+            SqlCommand cmd = new SqlCommand("sp_RegisterUser", lConnection);
 
             try
             {
-                //cmd.CommandType = CommandType.StoredProcedure;
-                //lConnection.Open();
+                cmd.CommandType = CommandType.StoredProcedure;
+                lConnection.Open();
+                cmd.Parameters.AddWithValue("@Pin", daUser.UserPin);
+                cmd.Parameters.AddWithValue("@UserName", daUser.UserName);
+                cmd.Parameters.AddWithValue("@Password", daUser.Password);
+                cmd.Parameters.AddWithValue("@FirstName", daUser.FirstName);
+                cmd.Parameters.AddWithValue("@LastName", daUser.LastName);
+                cmd.Parameters.AddWithValue("@Email", daUser.EmailAddress);
+                cmd.Parameters.AddWithValue("@BirthDate", daUser.BirthDate);
+                cmd.Parameters.AddWithValue("@StreetAddress", daUser.StreetAddress);
+                cmd.Parameters.AddWithValue("@City", daUser.City);
+                cmd.Parameters.AddWithValue("@State", daUser.State);
+                cmd.Parameters.AddWithValue("@ZipCode", daUser.ZipCode);
+                cmd.Parameters.AddWithValue("@PhoneNumber", daUser.PhoneNumber);
+                cmd.Parameters.AddWithValue("@SecurityQuestion", daUser.SecurityQuestion);
+                cmd.Parameters.AddWithValue("@SecurityAnswer", daUser.SecurityAnswer);
 
+                cmd.ExecuteNonQuery();
+                
             }
             catch (SqlException error)
             {
                 using (StreamWriter lWriter = new StreamWriter(_FileLocation, true))
                 {
-
+                    lWriter.WriteLine(error.Message);
                 }
+            }
+            finally
+            {
+                lConnection.Close();
             }
         }
 
